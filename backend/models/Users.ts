ca33b006ba789dfaedcf484dfe165e33bb81ce74
Konstantin <k.ognev709@mail.ru>
2024-10-18 22:06:09 +0600

    Добавлена схема пользователя

---
+++ backend/models/Users.ts
@@ -0,0 +1,69 @@
+import * as mongoose from 'mongoose';
+import bcrypt from 'bcrypt'
+
+const Schema = mongoose.Schema;
+const SALT_WORK_FACTOR = 10;
+
+export interface IUser {
+  _id: string;
+  email: string;
+  displayName: string;
+  avatar: string;
+  password: string;
+  role: 'user' | 'admin';
+  token: string;
+}
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  displayName:{
+    type: String,
+    unique: true,
+  },
+  avatar:{
+    type: String,
+    required: true,
+  },
+  password:{
+    type: String,
+    required: true,
+  },
+  role:{
+    type:String,
+    required: true,
+    default:'user',
+    enum:['user', 'admin'],
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  googleId:{
+    type: String,
+  }
+})
+
+UserSchema.pre('save' , async function (next){
+  if(!this.isModified('password')){
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR)
+  this.password = await bcrypt.hash(this.password , salt)
+
+  next()
+})
+
+UserSchema.set('toJSON', {
+  transform: (_doc , ret) =>{
+    delete ret.password;
+    return ret;
+  }
+})
+
+const User = mongoose.model('User' , UserSchema);
+export default User;
