95da8cbf3a468063be22dc65c3ffd1a96032eca7
Konstantin <k.ognev709@mail.ru>
2024-10-19 12:48:41 +0600

    Исравлена валидация создания и добавлен новый роут для получения всех постов

--- backend/router/photoRouter.ts
+++ backend/router/photoRouter.ts
@@ -6,8 +6,20 @@ import Photo from '../models/PhotoPublish';
 const photoRouter = express.Router();
 photoRouter.use(express.json());
 
+photoRouter.get('/' , async (req, res) => {
+    const allPhotos = await Photo.find().populate('userId' , 'displayName')
+    return res.status(200).json(allPhotos)
+})
+
 photoRouter.post('/', imagesUpload.single('photo') , async (req, res, next) => {
     try{
+        const { userId, title } = req.body;
+        const photoName = req.file?.filename;
+
+        if (!userId || !title || !photoName) {
+            return res.status(400).send({error: 'The fields should not be empty'});
+        }
+
         const photo = new Photo({
             userId: req.body.userId,
             title: req.body.title,
